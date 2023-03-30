9a70d080fbb52f2232faf4193089fa8bf46cd6c4
munkelb1 <marc.unkelbach@smail.th-koeln.de>
2023-03-30 16:34:18 +0200

    display weights for all numbers

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let allWeights;
 let squareSize = {x: 28, y:28};
 let numberOfSymbols = 10;
 let trainingDone = false;
+let microSquareSize = 3;
 
 function preload() {
     startPerceptron();
@@ -10,20 +11,23 @@ function preload() {
 
 function setup() {
     // p5.js stuff
-    createCanvas(1000, 800);
+    createCanvas(1200, 800);
     frameRate(1);
     colorMode(HSB, 360, 1, 1, 1);
     background(0, 0, 1);
+    textSize(12);
 
 }
 
 function draw() {
     background(0, 0, 1);
     if (trainingDone) {
-        drawWeights(2);
-        push();
-        translate(0, 350);
-        drawWeights(3);
+        text("Weights for numbers:",50, 20);
+        for (let i = 0; i < outputSize; i++) {
+            drawWeights(i);
+            push();
+        translate(100, 0);
+        }
         pop();
     } else {
         text('loading', 50,50);
@@ -32,15 +36,14 @@ function draw() {
 
 function drawWeights(number) {
     let minAndMax = getMinAndMaxForNumberArray(allWeights, 2);
-    let infoText = 'Weights for number ' + number + ':';
-    text(infoText, 50, 30);
+    text(number, 50, 40);
     let counter = number;
     for (let x = 0; x < squareSize.x; x++) {
         for (let y = 0; y < squareSize.y; y++) {
             let squareAlpha = allWeights[counter];
             let mappedSquareAlpha = map(squareAlpha, minAndMax.min, minAndMax.max,0,1);
             fill(0, 1, 0, mappedSquareAlpha);
-            square(50 + x * 10, 50 + y * 10, 10);
+            square(50 + x * microSquareSize, 50 + y * microSquareSize, microSquareSize);
             counter += outputSize;
         }
     }
